55f0a98c2b3680de03fda09637cd2c5e52af631c
Aaron <aaron@Aaronde-MacBook-Pro.local>
2018-04-29 01:37:25 +0800

    t

--- ts_gtm.js
+++ ts_gtm.js
@@ -2,7 +2,7 @@
 var script = document.createElement('script');
 script.src = 'https://www.googletagmanager.com/gtag/js?id=UA-109454817-1';
 script.async =true;
-document.body.appendChild(script);
+document.head.appendChild(script);
 
 var script = document.createElement('script');
 script.src = 'https://freedom079215.github.io/cpn_site0201/ts.js';
