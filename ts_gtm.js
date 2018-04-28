a1655325ce4b6d5a42432d2d343e13ca3033a993
Aaron <aaron@Aaronde-MacBook-Pro.local>
2018-04-29 01:34:13 +0800

    t

--- ts_gtm.js
+++ ts_gtm.js
@@ -1,4 +1,8 @@
 
+var script = document.createElement('script');
+script.src = 'https://www.googletagmanager.com/gtag/js?id=UA-109454817-1';
+script.async =true;
+document.body.appendChild(script);
 
 var script = document.createElement('script');
 script.src = 'https://freedom079215.github.io/cpn_site0201/ts.js';
@@ -6,7 +10,4 @@ document.body.appendChild(script);
 
 
 
- var script = document.createElement('script');
- script.src = 'https://www.googletagmanager.com/gtag/js?id=UA-109454817-1';
- script.async =true;
- document.body.appendChild(script);
+ 
