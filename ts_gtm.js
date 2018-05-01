42e9d7f43e5bb82a0874dd15d76253970e02935e
Aaron <aaron@Aaronde-MacBook-Pro.local>
2018-05-01 23:05:14 +0800

    t

--- ts_gtm.js
+++ ts_gtm.js
@@ -4,6 +4,13 @@ script.src = 'https://www.googletagmanager.com/gtag/js?id=UA-109454817-1';
 script.async =true;
 document.head.appendChild(script);
 
+window.dataLayer = window.dataLayer || [];
+
+function gtag(){dataLayer.push(arguments);}
+
+gtag('js', new Date());
+
+
 var script = document.createElement('script');
 script.src = 'https://freedom079215.github.io/cpn_site0201/ts.js';
 document.head.appendChild(script);
