82bf4dfe0894c53aa272764d239d35ad845c5edb
Aaron <aaron@Aaronde-MacBook-Pro.local>
2018-05-01 22:54:51 +0800

    t

--- ts_gtm.js
+++ ts_gtm.js
@@ -1,7 +1,6 @@
 
 var script = document.createElement('script');
 script.src = 'https://www.googletagmanager.com/gtag/js?id=UA-109454817-1';
-script.async =true;
 document.head.appendChild(script);
 
 var script = document.createElement('script');
